67826795550da701660292387ec74a6181b6172f
Lloyd Everett <lloydeverett@gmail.com>
2026-05-31 11:03:04 +0200

    compile flags script

---
+++ compile_flags.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+shopt -s nullglob
+
+ROOT="$(realpath $(pwd)/)"
+BOOST_INCLUDE_DIRS="$(echo $ROOT/build/_deps/boost-src/libs/*/include $ROOT/incrementalist-native/build/_deps/boost-src/libs/*/*/include)"
+
+echo "\
+-std=c++23
+-I$ROOT/build/_deps/ftxui-src/include
+$(printf "--" "-I%s\n" $BOOST_INCLUDE_DIRS)
+" > compile_flags.txt
+
+cat compile_flags.txt
+
+rm -f ./build/compile_commands.json
+
