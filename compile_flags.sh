1879307ae237ac8184e7a0b7f97c5be66b99eeff
Lloyd Everett <lloydeverett@gmail.com>
2026-06-27 13:06:18 +0200

    generate compile_commands.txt as part of build on non-MSVC

--- compile_flags.sh
+++ compile_flags.sh
@@ -6,15 +6,11 @@ shopt -s nullglob
 ROOT="$(realpath $(pwd)/)"
 BOOST_INCLUDE_DIRS="$(echo $ROOT/build/_deps/boost-src/libs/*/include $ROOT/build/_deps/boost-src/libs/*/*/include)"
 
-echo "writing compile_flags.txt..."
-
 echo "\
 -std=c++23
 -I$ROOT/build/_deps/ftxui-src/include
 $(printf "--" "-I%s\n" $BOOST_INCLUDE_DIRS)
 " >compile_flags.txt
-
-cat compile_flags.txt
 
 echo "wrote compile_flags.txt"
 
